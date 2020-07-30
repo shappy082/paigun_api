94eea59f0dfa4a739b06e07c5f2a6c18dfa6c1e6
thelegendman26 <teerapong.pu@baac.or.th>
2020-07-30 13:24:24 +0700

    edit model

--- models/userFaceModel.js
+++ models/userFaceModel.js
@@ -3,8 +3,11 @@ const Schema = mongoose.Schema;
 
 const userFaceSchema = Schema({
   username:  { type: String, required: true, trim: true },
-  password: { type: String, required: true, trim: true  },
-  user_id: { type: Number, required: true, trim: true  }
+  userid:  { type: Number, required: true, trim: true },
+  name:  { type: String, required: true, trim: true },
+  password: { type: String, required: true, trim: true , minlength: 3 },
+  role: { type: String, default: 'member' },
+  roledesc: { type: String, default: 'member' }
 },{
   collection: 'User_Authen'
 });
