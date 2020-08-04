a60010f8f6794f496b57123d134ade030181ca6b
papawich.in <papawich.in@baac.or.th>
2020-08-04 16:19:56 +0700

    update about user id and controller

--- models/locationModel.js
+++ models/locationModel.js
@@ -5,11 +5,12 @@ const schema = new Schema(
   {
     location_id: { type: Number, required: true },
     tag: { type: Array },
-    location_name: { type: String, required: true},
+    location_name: { type: String, required: true },
     location: [String]
   },
   {
     collection: "location_tb",
+    versionKey: false
   }
 );
 const location = mongoose.model("location_tb", schema);
