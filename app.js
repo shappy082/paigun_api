9e5ff562f114f4ea008b9e07a27976de008b5bee
papawich.in <papawich.in@baac.or.th>
2020-08-04 16:17:38 +0700

    code formmatted

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-//const helmet = require('helmet');
-//const rateLimit = require("express-rate-limit");
 const cors = require('cors');
 
 const passport = require('passport');
@@ -14,14 +12,9 @@ const { errorHandler } = require('./middleware/errorHandler');
 const config = require('./config/index');
 const connectDB = require('./config/db')
 
-// const userRoute = require('./routes/userRoute');
-// const postRoute = require('./routes/postRoute');
-// const friendReqRoute = require('./routes/friendReqRoute');
-// const commentRoute = require('./routes/commentRoute');
-const userFaceRoute = require('./routes/userFaceRoute');
+const userRoute = require('./routes/userRoute');
 const planningRoute = require('./routes/planningRoute');
 const locationRoute = require('./routes/locationRoute');
-const userProfileRoute =require('./routes/userProfileRoute');
 const app = express();
 
 //console.log(process.env);
@@ -33,23 +26,7 @@ app.use(logger('dev'))
 app.use(logHandler);
 
 connectDB(); 
-/*
-// connect to local database
-mongoose.connect(config.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}).then(() => console.log("Successfully connect to MongoDB."))
-    .catch(err => console.error("Connection error", err));
 
-// connect to Atlast DB
-mongoose.connect('mongodb+srv://dbuser02:dbuser02@cluster0-z4eg1.gcp.mongodb.net/codecamp?retryWrites=true&w=majority', {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}); */
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -58,14 +35,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 //init passport
 app.use(passport.initialize());
 
-app.use('/signin', userFaceRoute);
-app.use('/signup', userProfileRoute);
+app.use('/user', userRoute);
 app.use('/location', locationRoute);
 app.use('/planning', planningRoute);
-// app.use('/api/user', userRoute);
-// app.use('/api/post', postRoute);
-// app.use('/friend', friendReqRoute);
-// app.use('/comment', commentRoute);
+
 app.use(errorHandler);
 
 app.listen(config.PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${config.PORT}`));
