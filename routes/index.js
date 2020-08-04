f414f0c943b880350088246eb16a159b40f23dae
papawich.in <papawich.in@baac.or.th>
2020-08-04 16:16:46 +0700

    removed unused js

--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const userRoute = require('./routes/userRoute');
-const postRoute = require('./routes/postRoute');
-
-module.exports = router
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,25 +1,19 @@
 const express = require('express')
 const { body } = require('express-validator');
 const userController = require('../controllers/userController')
-const userProfileController = require('../controllers/userProfileController')
 const authentication = require('../middleware/authenticationHandler');
 const authorization = require('../middleware/authorizationHandler');
 
 const router = express.Router()
-
+router.post('/signin', [
+  body('username').not().isEmpty().withMessage('Field username is required'),
+  body('password').not().isEmpty().withMessage('Field password is required')
+], userController.signin);
 router.post('/signup', [
-    body('username').not().isEmpty().withMessage('Field username is required'),
-    body('password').not().isEmpty().withMessage('Field password is required').isLength({ min: 6 }).withMessage('Password must be  at least 6 digits')
-], userProfileController.signup);
-router.post('/signin',
-    body('name').not().isEmpty().withMessage('Field name is required'),
-    body('password').not().isEmpty().withMessage('Field password is required')
-    , userController.signin);
-router.get('/me', authentication.isLoggedIn, userController.getProfile);
+  body('username').not().isEmpty().withMessage('Field username is required'),
+  body('password').not().isEmpty().withMessage('Field password is required'),
+  body('name').not().isEmpty().withMessage('Field name is required')
+], userController.signup);
 
-router.get('/', userController.index);
-router.get('/:id', authentication.isLoggedIn, userController.getUserById);
-router.put('/:id', authentication.isLoggedIn, userController.updateUser);
-router.delete('/:id', [authentication.isLoggedIn, authorization.isAdmin], userController.deleteUser);
 module.exports = router
 
